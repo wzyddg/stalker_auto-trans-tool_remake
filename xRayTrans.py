e0371d0f66d71a5f53d1f0c52dac17e32c5e60de
shepard.wang <shepard.wang@ebaotech.com>
2022-07-10 22:48:44 +0800

    rm

--- xRayTrans.py
+++ xRayTrans.py
@@ -44,7 +44,7 @@ def main(argv):
         if opt in ("-h", "--help"):
             helpText = """
 S.T.A.L.K.E.R. Game(X-Ray Engine) Text Auto-Translator, using with language pack generator is recommended.
-Version 2.0.2, Updated at Jul 10th 2022
+Version 2.0.2, Updated at Jul 11th 2022
 by wzyddgFB from baidu S.T.A.L.K.E.R. tieba
 
 Options:
