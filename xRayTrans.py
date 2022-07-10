dd81659f40c9de68d619d29f56d1581efc64498d
shepard.wang <shepard.wang@ebaotech.com>
2022-07-10 16:33:30 +0800

    pretty help， start script

--- xRayTrans.py
+++ xRayTrans.py
@@ -48,20 +48,28 @@ Version 2.0.2, Updated at Jul 10th 2022
 by wzyddgFB from baidu S.T.A.L.K.E.R. tieba
 
 Options:
-  -e <value>|--engine=<value>                   use what translate engine. eg: baidu qq deepl
-  -p <value>|--path=<value>                     path of target folder, always quote with ""
-  -t <value>|--toLang=<value>                   translate to what language. eg: eng chs
-  -i <value>|--appId=<value>                    (Optional)appId of engine, required for baidu
-  -k <value>|--appKey=<value>                   (Optional)appKey of engine, required for baidu
-  -f <value>|--fromLang=<value>                 (Optional)from what language if string uses text tag, default rus. eg: eng rus ukr
-  -a <value>|--analyzeCharCount=<value>         (Optional)more than how many chars does the sentence need qq analyze,
-                                                    default 0, means don't analyze(qq engine only)
-  -r <value>|--reusePath=<value>                (Optional)path of existing translated xml folder.
-                                                    for gameplay translating text id protecting or text translating accelerating
-  -c        |--runnableCheck                    (Optional)just analyze files, won't do translation
-  --forceTransFiles=<value>                     (Optional)for listed files, ignore existing translated xml files.
-                                                    concat with ','. eg: a.xml,b.xml
-  --function=<value>                            (Optional)translating function, default text. eg: text gameplay
+  -e <value>|--engine=<value>               use what translate engine.
+                                                eg: baidu qq deepl
+  -p <value>|--path=<value>                 path of target folder.
+                                                always quote with ""
+  -t <value>|--toLang=<value>               translate to what language.
+                                                eg: eng chs
+  -i <value>|--appId=<value>                (Optional)appId of engine.
+                                                required for baidu
+  -k <value>|--appKey=<value>               (Optional)appKey of engine.
+                                                required for baidu
+  -f <value>|--fromLang=<value>             (Optional)from what language.
+                                                default rus. eg: eng rus ukr
+  -a <value>|--analyzeCharCount=<value>     (Optional)more than how many chars does the sentence need qq analyze.
+                                                default 0, means don't analyze(qq engine only)
+  -r <value>|--reusePath=<value>            (Optional)path of existing translated xml folder.
+                                                for gameplay translating text id protecting or text translating accelerating, always quote with ""
+  -c        |--runnableCheck                (Optional)just analyze files.
+                                                won't do translation.
+  --forceTransFiles=<value>                 (Optional)files that ignore reuse.
+                                                concat with ','. eg: a.xml,b.xml
+  --function=<value>                        (Optional)translating function.
+                                                default text. eg: text gameplay script
         """
             print(helpText)
             sys.exit()
@@ -232,6 +240,9 @@ Options:
                     xRayXmlParser.generateOutputXmlFromString(os.path.join(
                         textDir, "translated_"+engine, xRFile), repdText)
                 print("")
+
+            elif transFunction == 'script':
+                pass
         else:
             print("\n"+fullPath+" is not a file or already existed.")
 
