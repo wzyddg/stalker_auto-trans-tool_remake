b42bab3ff841b8da1e06ec51ed0c9fd3684c1e41
shepard.wang <shepard.wang@ebaotech.com>
2022-11-21 00:09:00 +0800

    widen header def

--- xRayXmlParser/xRayTextAnalyzer.py
+++ xRayXmlParser/xRayTextAnalyzer.py
@@ -208,7 +208,7 @@ def normalize_xml_string(xmlStr: str, needFixST: bool = True, deleteHeader: bool
     replaced = re.sub(r'<!--[\s\S]*?-->', '', replaced)
 
     if deleteHeader:
-        replaced = re.sub(r'<\?xml[^>]+encoding[=\s][^>]+\?>', '', replaced)
+        replaced = re.sub(r'<\?[^>]+\?>', '', replaced)
 
     # convert < in xml, but not tag
     replaced = re.sub('<(?![A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD\\u10000-\\uEFFFF/])', '&lt;', replaced).strip()
