ca846a1190a83c9830cee883cb0d8296a7682b78
shepard.wang <shepard.wang@ebaotech.com>
2022-11-22 22:27:17 +0800

    去除w3c标准中对xml开始标签的过度宽容（不认识的一概允许）

--- xRayXmlParser/xRayTextAnalyzer.py
+++ xRayXmlParser/xRayTextAnalyzer.py
@@ -214,7 +214,7 @@ def normalize_xml_string(xmlStr: str, needFixST: bool = True, deleteHeader: bool
         replaced = re.sub(r'<\?[^>]+\?>', '', replaced)
 
     # convert < in xml, but not tag
-    replaced = re.sub('<(?![A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD\\u10000-\\uEFFFF/])', '&lt;', replaced).strip()
+    replaced = re.sub('<(?![A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD/])', '&lt;', replaced).strip()
     if replaced.startswith("&lt;?xml"):
         replaced = "<"+replaced[4:]
 
